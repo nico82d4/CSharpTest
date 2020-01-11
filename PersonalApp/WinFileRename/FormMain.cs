ce2ec6ce494e590a7afaf96c5375124f253f17d3
nico82d4 <nico421091@gmail.com>
2020-01-11 22:47:26 +0900

    Gitの-aオプションが効いていないみたいで、git add しなおした

---
+++ PersonalApp/WinFileRename/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFileRename
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonExecute_Click(object sender, EventArgs e)
+        {
+            TextBoxProgress.Text = this.SetAppPath();
+        }
+
+        private string SetAppPath()
+        {
+            string returnValue = Application.ExecutablePath;
+
+            return returnValue;
+        }
+    }
+}
