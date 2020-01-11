d1461d8e949d9a1adc1c8eb4d1c4206e29fae4e1
nico82d4 <nico421091@gmail.com>
2020-01-12 00:26:43 +0900

    ファイルの一覧取得処理まで完了

--- PersonalApp/WinFileRename/FormMain.cs
+++ PersonalApp/WinFileRename/FormMain.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
 
 namespace WinFileRename
 {
@@ -12,13 +14,55 @@ namespace WinFileRename
 
         private void ButtonExecute_Click(object sender, EventArgs e)
         {
-            TextBoxProgress.Text = this.SetAppPath();
+            TextBoxProgress.Text = this.GetFileList();
         }
 
         private string SetAppPath()
         {
+            // アプリケーションの実行パスを取得
             string returnValue = Application.ExecutablePath;
 
+            return returnValue;
+        }
+
+
+        // ファイルの一覧を文字列で返す処理
+        private string GetFileList()
+        {
+            string returnValue = "";
+
+            try
+            {
+                // アプリの実行パスを取得し、ファイルの一覧を取得する。
+                string searchPath = Path.GetDirectoryName(Application.ExecutablePath);
+                DirectoryInfo dirInfo = new DirectoryInfo(@searchPath);
+                IEnumerable<FileInfo> infos = dirInfo.EnumerateFiles();
+
+                // foreachループで、個別のファイル名をフルパスで取得し、変数に格納する。
+                // パスとパスとの間には、改行コードを挿入する。Windows用なのでCrLf。
+                foreach (var info in infos)
+                {
+                    returnValue += info.FullName + "\r\n";
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // 戻り値を返す。
             return returnValue;
         }
     }
