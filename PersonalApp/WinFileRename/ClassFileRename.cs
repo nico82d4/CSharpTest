894ce8a5ef23ac8af5c792482b83cf134f89975b
nico82d4 <nico421091@gmail.com>
2020-01-12 16:20:42 +0900

    実処理用のクラスを再作成して、FormMainのソースを移動。README.mdの文言も若干修正。

---
+++ PersonalApp/WinFileRename/ClassFileRename.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFileRename
+{
+    class ClassFileRename
+    {
+        // 実行結果文字列を格納する読み出し専用プロパティ
+        public string ResultString { get; private set; } = "";
+
+        // エラーメッセージを格納する読み出し専用プロパティ
+        public string ErrorMessage { get; private set; } = "";
+
+
+
+        // ファイルリネームの実行
+        public Boolean Execute(string path)
+        {
+            // 戻り値の初期値をFalseとする。
+            Boolean returnValue = false;
+
+            try
+            {
+                // ディレクトリが存在する場合
+                if (Directory.Exists(path))
+                {
+                    // ファイルの一覧を取得
+                    DirectoryInfo dirInfo = new DirectoryInfo(@path);
+                    IEnumerable<FileInfo> lists = dirInfo.EnumerateFiles();
+
+                    // 取得件数が0件より大きい場合
+                    if (lists.Count() > 0)
+                    {
+                        // ループ処理で個別ファイルの対応を行う
+                        foreach (var lst in lists)
+                        {
+                            // ファイル名をフルパスで取得する。
+                            this.ResultString += lst.FullName + "\r\n";
+                        }
+
+                        // 2020-01-12時点では、結果文字列が格納されていれば、
+                        // 処理成功とする。
+                        if (this.ResultString != "")
+                        {
+                            returnValue = true;
+                        }
+                    }
+
+                    // ファイル数が0件の場合
+                    else
+                    {
+                        ErrorMessage = "ディレクトリの中にファイルがありませんでした。";
+                    }
+                }
+
+                // 指定されたディレクトリが存在しない場合
+                else
+                {
+                    ErrorMessage = "指定されたディレクトリが存在しません。";
+                }
+            }
+
+            // 例外が発生した時は、例外メッセージをエラー文字列に格納する。
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
+
+            // 戻り値を返す。
+            return returnValue;
+
+        }
+    }
+}
--- PersonalApp/WinFileRename/FormMain.cs
+++ PersonalApp/WinFileRename/FormMain.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Windows.Forms;
-using System.Collections.Generic;
 using System.IO;
+using System.Windows.Forms;
 
 namespace WinFileRename
 {
@@ -13,60 +12,28 @@ namespace WinFileRename
         }
 
 
+
         // 実行ボタンを押下した時の処理
         private void ButtonExecute_Click(object sender, EventArgs e)
         {
-            var list = GetFileList();
-            string str = "";
+            // プログラム実行ディレクトリの取得
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
 
-            foreach (var lst in list)
+            // ファイルリネーム実行クラスのインスタンスを生成し、
+            // 実行する。
+            ClassFileRename fr = new ClassFileRename();
+            Boolean result = fr.Execute(path);
+
+            // 実行結果がTrueなら、テキストボックスに結果を出力する。
+            if (result)
             {
-                str += lst + "\r\n";
+                TextBoxProgress.Text = fr.ResultString;
             }
-
-            TextBoxProgress.Text = str;
-        }
-
-
-
-        // ファイルの一覧を文字列で返す処理
-        private List<string> GetFileList()
-        {
-            var lst = new List<string>();
-
-            try
+            // 実行結果がFalseの場合は、エラーメッセージを出力する。
+            else
             {
-                // アプリの実行パスを取得し、ファイルの一覧を取得する。
-                string searchPath = Path.GetDirectoryName(Application.ExecutablePath);
-                DirectoryInfo dirInfo = new DirectoryInfo(@searchPath);
-                IEnumerable<FileInfo> infos = dirInfo.EnumerateFiles();
-
-                // foreachループで、個別のファイル名をフルパスで取得し、変数に格納する。
-                // パスとパスとの間には、改行コードを挿入する。Windows用なのでCrLf。
-                foreach (var info in infos)
-                {
-                    lst.Add(info.FullName);
-                }
+                MessageBox.Show(fr.ErrorMessage, "エラー");
             }
-            catch (ArgumentException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            catch (IOException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-            // 戻り値を返す。
-            return lst;
         }
     }
 }
